a5e1ecbed3ac6cd232020aa06defaa4b854a620d
angocc11 <angoc1234567890@gmail.com>
2026-08-19 22:33:57 +0700

    feat: implement cart APIs

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,9 @@ app.use(express.json());
 // Import & sử dụng Auth Routes
 const authRoutes = require('./routes/authRoutes');
 app.use('/api/auth', authRoutes);
-
+// Cart Routes
+const cartRoutes = require('./routes/cartRoutes');
+app.use('/api/cart', cartRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
